d40e7fdded3dedbc08fa5563d7574e36464a2380
Cameron Cashwell <cdevconsulting@protonmail.com>
2018-06-26 18:15:53 -0400

    check

--- global.js
+++ global.js
@@ -1,34 +1,15 @@
-//var json = {
-//    'files': [{
-//            "Name": "zips",
-//            "Type": "Directory",
-//            "DateModified": "6/14/2018 17:22:50",
-//            "Size": "2 KB",
-//        }, {
-//            "Name": "presets",
-//            "Type": "Directory",
-//            "DateModified": "5/11/2018 7:32:10",
-//            "Size": "6 KB",
-//        }, {
-//            "Name": "workflow",
-//            "Type": "Directory",
-//            "DateModified": "6/26/2018 10:29:59",
-//            "Size": "6 KB",
-//        },
-//    ]
-//}
 //window.addEventListener('load', function() {
 
     var json =[{
                 "Name": "zips",
                 "Type": "Directory",
                 "DateModified": "6/14/2018 17:22:50",
-                "Size": "2 KB",
+                "Size": "5 KB",
             }, {
                 "Name": "presets",
                 "Type": "Directory",
                 "DateModified": "5/11/2018 7:32:10",
-                "Size": "6 KB",
+                "Size": "2 KB",
             }, {
                 "Name": "workflow",
                 "Type": "Directory",
@@ -43,5 +24,11 @@
         string +='<div class="row"><div class="col-md-15 col-sm-1"><input type="checkbox" id="all" name="all" value="all"></div><div class="col-md-15 col-sm-4"><span class="folders">'+json[i].Name+'</span></div><div class="col-md-15 col-sm-3"><span class="directory">'+json[i].Type+'</span></div><div class="col-md-15 col-sm-3"><span class="date-stamp">'+json[i].DateModified+'</span></div><div class="col-md-15 col-sm-1"><span class="date-size">'+json[i].Size+'</span></div></div>';
     };
     document.getElementsByClassName('update-data')[0].innerHTML =string
+
+    json.sort(function(a, b) {
+        return parseFloat(a.Size) - parseFloat(b.Size);
+    });
     
-//});
+//    document.getElementById('size').addEventListener('click', function () {
+//        json.sort(function)(a,b);
+//    });    
